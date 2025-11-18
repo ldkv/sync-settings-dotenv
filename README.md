551b58aa31dad4d58f716e317e094dba6ca887af
Vu LE <ledkvu@gmail.com>
2025-11-18 16:59:07 +0100

    Support sync between dotenv files (#4)

--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 
 # sync-settings-dotenv
 
-A Python CLI to sync configs from code to dotenv file.
+A Python CLI to sync configs between dotenv files and Python settings module.
+
+It allows to define your application settings in a Python module and use it as a source of truth, instead of maintaining both the settings module and multiple dotenv files manually.
 
 Currently only supports BaseSettings model from [pydantic-settings](https://docs.pydantic.dev/latest/concepts/pydantic_settings/).
 
@@ -19,15 +21,51 @@ The tool can be installed using [uv tool](https://docs.astral.sh/uv/guides/tools
 uv tool install sync-settings-dotenv
 ```
 
-After installation, it will be available as `sync-dotenv` command in your terminal.
+After installation, it will be available as `sync-settings-dotenv` command in your terminal.
 
 ## Usage
 
 For details on usage, run:
 
 ```bash
-sync-dotenv --help
+sync-settings-dotenv --help
 ```
+
+### Generate a .env.example from Python settings file
+
+To generate a `.env.example` file from a Python settings file, run:
+
+```bash
+sync-settings-dotenv path.to.settings.Module path/to/.env.example --exact
+```
+
+This command will create a `.env.example` file with all environment variables defined in the specified settings module, using their default values.
+
+A backup of the previous version will be created as `.env.example.backup.timestamp` if the file already exists.
+
+### Sync from Python settings file to .env file
+
+```bash
+sync-settings-dotenv path.to.settings.Module /path/to/.env
+```
+
+This command will generate or update the existing `.env` file with default values from the specified settings module. If the `.env` file already exists, it will only add missing variables without overwriting existing ones.
+
+To overwrite existing values in the `.env` file, use the `--overwrite-values` flag:
+
+```bash
+sync-settings-dotenv path.to.settings.Module /path/to/.env --overwrite-values
+```
+
+### Sync from .env.example file to .env file
+
+It is also possible to sync from an existing `.env.example` file to a `.env` file:
+
+```bash
+sync-settings-dotenv /path/to/.env.example /path/to/.env
+```
+
+It works the same way as syncing from a Python settings file.
 
 ## Update and Uninstallation
 
--- src/sync_settings_dotenv/main.py
+++ src/sync_settings_dotenv/main.py
@@ -1,66 +1,69 @@
 from pathlib import Path
-from typing import Annotated, Type
+from typing import Annotated
 
 import typer
 from dotenv import dotenv_values
-from pydantic_settings import BaseSettings
 from rich import print as rprint
 
-from sync_settings_dotenv.utils import generate_dotenv_file, import_class_from_module, serialize_default_env_vars
+from sync_settings_dotenv.utils import generate_dotenv_file, generate_source_env_vars, resolve_env_vars_combination
+
+app = typer.Typer(help="A CLI tool to sync environment variables in dotenv files or from Python settings files.")
 
 
-def update_env_file(settings_model: Type[BaseSettings], env_path: Path, overwrite: bool = False) -> None:
-    env_vars = dotenv_values(env_path)
-    overwritten_keys = set()
-    for field_name, default_value in serialize_default_env_vars(settings_model).items():
-        field_name_upper = field_name.upper()
-        if not overwrite and field_name_upper in env_vars:
-            continue
-
-        existing_value = env_vars.get(field_name_upper)
-        if existing_value != default_value:
-            overwritten_keys.add(field_name_upper)
-
-        env_vars[field_name_upper] = default_value
-
-    if not overwritten_keys:
-        rprint("[blue]Info:[/blue] No environment variables were overwritten.")
-        return
-
-    generate_dotenv_file(env_path, env_vars)
-
-
+@app.command()
 def sync_settings_dotenv(
-    module_path: str = typer.Argument(..., help="Full path to the module containing the Pydantic BaseSettings model."),
-    env_path: Annotated[
-        Path,
-        typer.Option(
-            ".env",
-            "--output",
-            "-o",
-            help="Output path for the generated .env file.",
+    src_path: Annotated[
+        str,
+        typer.Argument(
+            ...,
+            help="Full path to the module with environment variables to sync. It can be either an .env file or a Python module path containing a Pydantic BaseSettings subclass.",
         ),
+    ],
+    dst_path: Annotated[
+        Path,
+        typer.Argument(..., help="Output path for the generated .env file."),
     ] = Path(".env"),
     overwrite_values: bool = typer.Option(
         False,
         "--overwrite-values",
-        help="Overwrite existing values in the .env file.",
+        help="Overwrite existing values in the .env file. Keep extra variables that are not in the source.",
+    ),
+    exact_match: bool = typer.Option(
+        False,
+        "--exact",
+        help="Match the .env file exactly to the source. Remove any extra variables not present in the source.",
+    ),
+    with_header: bool = typer.Option(
+        True,
+        "--header/--no-header",
+        help="Include a header comment in the .env file.",
     ),
 ):
-    """Generate an .env file from a Pydantic BaseSettings model."""
+    """
+    Sync environment variables from a source to a target .env file.
+
+    The function generates or updates the target .env file based on the source, with options to overwrite values or exact match.
+    """
     try:
-        settings_model = import_class_from_module(module_path)
+        src_env_vars = generate_source_env_vars(src_path)
     except ImportError as e:
-        rprint(f"[red]Error:[/red] Unable to import settings model from {module_path=}. Error: {e}")
+        rprint(f"[red]Error:[/red] Unable to import settings model from {src_path=}. Error: {e}")
         raise typer.Exit(code=1) from e
 
-    update_env_file(settings_model, env_path, overwrite=overwrite_values)
-    rprint(f"[green]Success:[/green] Generated .env file at '{env_path}'.")
+    dst_env_vars = dotenv_values(dst_path)
+    combined_env_vars = resolve_env_vars_combination(
+        src_env_vars,
+        dst_env_vars,
+        overwrite_values=overwrite_values,
+        exact_match=exact_match,
+    )
+    if combined_env_vars == dst_env_vars:
+        rprint("[blue]Info:[/blue] No changes detected. Skipping update.")
+        return
 
-
-def main():
-    typer.run(sync_settings_dotenv)
+    generate_dotenv_file(dst_path, combined_env_vars, with_header=with_header)
+    rprint(f"[green]Success:[/green] Generated .env file at '{dst_path}'.")
 
 
 if __name__ == "__main__":
-    main()
+    app()
